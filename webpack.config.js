ec6afeca79e5d0220dcc9596a2fcb5cb39359811
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-03-07 00:26:52 +0100

    Finished basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    devtool: 'inline-source-map', // for finding errors
+    devServer: {
+        static: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // for updating html each time i run build
+        title: 'Development',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    optimization: { // added coz i have more than one entrypoint on a single HTML page.
+        runtimeChunk: 'single',
+    },
+}
